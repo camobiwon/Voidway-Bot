614d343bd05ca232152bc157367c9e2dae4ec0a6
extraes <52384576+extraes@users.noreply.github.com>
2023-02-03 14:16:37 -0800

    Fix compiler bitching Not supposed to null check using equality operator, use "is not null" and "is null" exclusively now... apparently.

--- Moderation.cs
+++ Moderation.cs
@@ -126,7 +126,7 @@ namespace Voidway_Bot {
             if (message.Author.IsBot)
                 return Task.CompletedTask;
 
-            if (pastMessage != null && pastMessage.Content == message.Content)
+            if (pastMessage is not null && pastMessage.Content == message.Content)
                 return Task.CompletedTask;
 
             string attachments = "";
@@ -136,14 +136,14 @@ namespace Voidway_Bot {
             DiscordEmbedBuilder embed = new() {
                 Title = $"Message {actionType}",
                 Color = DiscordColor.Gray,
-                Description = $"{(!string.IsNullOrEmpty(message.Content) ? "**Message**\n" + message.Content : "")} {(pastMessage != null ? "\n\n**Past Message**\n" + pastMessage.Content : (!string.IsNullOrEmpty(attachments) ? "\n\n**Attachments**" + attachments : ""))}",
+                Description = $"{(!string.IsNullOrEmpty(message.Content) ? "**Message**\n" + message.Content : "")} {(pastMessage is not null ? "\n\n**Past Message**\n" + pastMessage.Content : (!string.IsNullOrEmpty(attachments) ? "\n\n**Attachments**" + attachments : ""))}",
                 Footer = new DiscordEmbedBuilder.EmbedFooter() { Text = $"User ID: {message.Author.Id} | Message ID: {message.Id}" }
             };
             embed.AddField("User", $"{message.Author.Username}", true);
             embed.AddField("Channel", $"<#{message.Channel.Id}>", true);
             embed.AddField("Original Time", $"<t:{message.Timestamp.ToUnixTimeSeconds()}:f>", true);
 
-            guild.GetChannel(Config.FetchMessagesChannel(guild.Id)).SendMessageAsync(embed);
+            guild.GetChannel(Config.FetchMessagesChannel(guild.Id))?.SendMessageAsync(embed);
             return Task.CompletedTask;
         }
 
