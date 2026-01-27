cbdad962700491fcbe1d29f451b7c8b18ae19a52
extraes <52384576+extraes@users.noreply.github.com>
2026-01-27 00:11:26 -0800

    partial commit -- pass back to macbook

--- Voidway/ModioEvents.cs
+++ Voidway/ModioEvents.cs
@@ -1,15 +1,32 @@
 using Modio;
+using Modio.Filters;
 using Modio.Models;
 
 namespace Voidway;
 
 public static class ModioEvents
 {
-    static async void FetchLoop(Client)
+    
+    public static async Task Init(Client clint)
+    {
+        try
+        {
+            var games = await clint.Games.Search().ToList();
+            var bonelabGame = games.First(g => g.NameId == "bonelab");
+            var bonelab = clint.Games[bonelabGame.Id];
+            var bonelabMods = bonelab.Mods;
+        }
+        FetchLoop(clint);
+    }
+    
+    static async void FetchLoop(ModsClient mods, CommentsClient comments)
     {
         while (true)
         {
-            await Task.Delay(5000);
+            await Task.Delay(60 * 1000);
+            
+            
+            
             try
             {
                 
@@ -17,7 +34,7 @@ public static class ModioEvents
             }
             catch (Exception ex)
             {
-                Logger.Warn()
+                Logger.Warn("Exception in Mod.IO fetch loop", ex);
             }
         }
         
